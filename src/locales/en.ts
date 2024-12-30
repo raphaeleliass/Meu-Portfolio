1c4c278068f5530d3e58c9b7353e8e9fa5c4032e
Raphael Elias <thefael98@gmail.com>
2024-12-30 12:22:56 -0300

    feat: add locales directory to site languages

---
+++ src/locales/en.ts
@@ -0,0 +1,20 @@
+export const en = [
+  {
+    hero: {
+      greetings: "Hello, I'm Raphael!",
+      title: "Web Developer, Data Analyst, Creator.",
+      buttons: {
+        repositories: "Meet my repositories",
+        projects: "Discover my projects",
+      },
+    },
+  },
+
+  {
+    AboutMe: {
+      title: "About me",
+      content:
+        "Student of Systems Analysis and Development, focused on front-end development and data analysis with Python. I have experience in React, TypeScript, NodeJS, and Python, as well as basic knowledge in SEO and UI/UX. I am looking for opportunities to improve my skills and start my career in the technology field, with the goal of becoming a full-stack developer and, in the future, exploring the international market.",
+    },
+  },
+];
