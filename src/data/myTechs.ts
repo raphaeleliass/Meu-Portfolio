2979b22c55787e34a41df0d1f9d59a487c2f5352
Raphael Elias <thefael98@gmail.com>
2024-12-30 12:21:27 -0300

    feat: add myTechs json file

---
+++ src/data/myTechs.ts
@@ -0,0 +1,54 @@
+export const myTechs = [
+  {
+    name: "HTML",
+    description: "Markup language",
+  },
+  {
+    name: "CSS",
+    description: "Style sheets",
+  },
+  {
+    name: "TailwindCSS",
+    description: "Utility-first CSS",
+  },
+  {
+    name: "JavaScript",
+    description: "Programming language",
+  },
+  {
+    name: "TypeScript",
+    description: "Typed JavaScript",
+  },
+  {
+    name: "ReactJS",
+    description: "UI library",
+  },
+  {
+    name: "NextJS",
+    description: "React framework",
+  },
+  {
+    name: "NodeJS",
+    description: "JavaScript runtime",
+  },
+  {
+    name: "Python",
+    description: "Programming language",
+  },
+  {
+    name: "Git",
+    description: "Version control",
+  },
+  {
+    name: "GitHub",
+    description: "Code hosting",
+  },
+  {
+    name: "SEO",
+    description: "Search optimization",
+  },
+  {
+    name: "UI/UX",
+    description: "Design principles",
+  },
+];
