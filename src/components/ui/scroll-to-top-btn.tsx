8f912190473985e6193f31ee49bdd9de1de48c66
Raphael Elias <thefael98@gmail.com>
2024-12-30 12:24:51 -0300

    feat: add scroll to top button

---
+++ src/components/ui/scroll-to-top-btn.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowUp } from "lucide-react";
+
+export default function ScrollToTopBtn() {
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  return (
+    <Button
+      className="fixed bottom-4 right-4 rounded-full"
+      variant={"secondary"}
+      onClick={scrollToTop}
+    >
+      <ArrowUp />
+    </Button>
+  );
+}
