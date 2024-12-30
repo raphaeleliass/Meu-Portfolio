84f80ff590dd54afdf2e6a6465255d3b34702c41
Raphael Elias <thefael98@gmail.com>
2024-12-30 18:09:01 -0300

    update: changes on structure to read easily

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
         protocol: "https",
-        hostname: "github.com"
-      }
-    ]
-  }
+        hostname: "github.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
