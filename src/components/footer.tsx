7b16f3b6a2f2e0711b69e9d44cb5f5e2e6fedddb
Raphael Elias <thefael98@gmail.com>
2024-12-30 17:55:05 -0300

    update: add my personal social medias to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,60 @@
+import { myLinks } from "@/data/myLinks";
 import React from "react";
+import {
+  SiGithub,
+  SiInstagram,
+  SiLinkedin,
+  SiWhatsapp,
+} from "@icons-pack/react-simple-icons";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="relative py-32">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h3 className="text-center font-Poppins text-2xl font-semibold md:text-3xl">
+          Let&apos;s connect!
+        </h3>
+        <span className="mt-4 flex flex-row gap-2">
+          {myLinks.map((link) => (
+            <a
+              className="p-2"
+              role="button"
+              key={link.title}
+              href={link.url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {link.title === "Github" && (
+                <SiGithub className="transition-all hover:size-8" />
+              )}
+              {link.title === "Whatsapp" && (
+                <SiWhatsapp className="transition-all hover:size-8" />
+              )}
+              {link.title === "Instagram" && (
+                <SiInstagram className="transition-all hover:size-8" />
+              )}
+              {link.title === "Linkedin" && (
+                <SiLinkedin className="transition-all hover:size-8" />
+              )}
+            </a>
+          ))}
+        </span>
+        <p className="mt-4 text-sm text-muted-foreground">
+          Find me on social media
+        </p>
+      </div>
+
+      <p className="absolute bottom-2 left-0 right-0 w-full text-center text-xs text-muted-foreground">
+        &copy; Created by{" "}
+        <a
+          className="hover:text-cyan-300 hover:underline hover:underline-offset-2"
+          href="https://github.com/raphaeleliass"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Raphael Elias
+        </a>
+      </p>
+    </footer>
+  );
 }
