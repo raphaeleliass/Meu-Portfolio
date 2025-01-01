50b126feb5fb0ae8e529d7ace6c581f03fea5c19
Raphael Elias <thefael98@gmail.com>
2025-01-01 20:05:57 -0300

    refactor: enhance Section component with generic props and add SectionDescription component

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -1,23 +1,46 @@
 import clsx from "clsx";
 import { AllHTMLAttributes, ReactNode, Ref } from "react";
 
-interface SectionProps extends AllHTMLAttributes<HTMLDivElement> {
+interface BaseProps<T extends HTMLElement> extends AllHTMLAttributes<T> {
   className?: string;
-  children?: ReactNode;
-  ref?: Ref<HTMLDivElement>;
+  children: ReactNode;
+  ref?: Ref<T>;
 }
 
-export const Section = ({ className, ref, ...props }: SectionProps) => (
+export const Section = ({
+  className,
+  ref,
+  ...props
+}: BaseProps<HTMLDivElement>) => (
   <div className={clsx("section", className)} ref={ref} {...props} />
 );
 
-export const SectionTitle = ({ className, ref, ...props }: SectionProps) => (
+export const SectionTitle = ({
+  className,
+  ref,
+  ...props
+}: BaseProps<HTMLHeadingElement>) => (
   <h2
     className={clsx(
-      "font-Poppins text-2xl font-semibold md:text-4xl",
+      "font-Poppins text-xl font-semibold md:text-3xl",
       className,
     )}
     {...props}
     ref={ref}
+  />
+);
+
+export const SectionDescription = ({
+  className,
+  ref,
+  ...props
+}: BaseProps<HTMLParagraphElement>) => (
+  <p
+    className={clsx(
+      "max-w-xs text-center text-sm text-muted-foreground md:max-w-sm",
+      className,
+    )}
+    ref={ref}
+    {...props}
   />
 );
