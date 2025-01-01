dc2676dfb5b9cfdc4b07885d1fb030329c4e5e25
Raphael Elias <thefael98@gmail.com>
2025-01-01 02:47:38 -0300

    feat: add theme provider and mode toggle to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,11 +17,14 @@ import { ArrowRight } from "lucide-react";
 
 // form component imports
 import WhatsappForm from "./forms/whatsapp-form";
+import { ModeToggle } from "./ui/mode-toggle";
 
 export default function Navbar() {
   return (
-    <header className="container fixed left-0 right-0 top-0 z-50 mx-auto flex w-full items-center justify-center bg-background px-4 py-6 md:static">
+    <header className="container fixed left-0 right-0 top-0 z-50 mx-auto flex w-full items-center justify-center bg-background px-4 py-6">
       <nav role="navigation" className="flex w-full flex-row justify-between">
+        <ModeToggle />
+
         <Dialog>
           <DialogTrigger asChild>
             <Button variant={"ghost"}>
