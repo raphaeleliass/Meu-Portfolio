bb64b9213a757bf91c55c01a092525038d27a49d
Raphael Elias <thefael98@gmail.com>
2025-01-03 22:53:56 -0300

    feat: implement admin authentication page with form and routing

--- src/app/(private)/admin/page.tsx
+++ src/app/(private)/admin/page.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+"use client";
+import AdminForm from "@/components/forms/admin-form";
+import { Section } from "@/components/ui/section";
+import { auth } from "@/firebase/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-export default function Admin() {
-  return <div>admin</div>;
+export default function Page() {
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setLoading(true);
+      if (user != null) {
+        router.push("/admin/dashboard");
+      } else {
+        setLoading(false);
+      }
+    });
+
+    return () => unsub();
+  }, [router]);
+  return (
+    <Section className="mx-auto">
+      {loading ? <Loader2 className="animate-spin" /> : <AdminForm />}
+    </Section>
+  );
 }
