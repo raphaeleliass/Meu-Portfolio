82594b4104506d385e4998d93ad8bf43727a7132
Raphael Elias <thefael98@gmail.com>
2025-01-06 13:42:24 -0300

    refactor: remove unused dashboard page and enhance AppSidebar with user context and dropdown menu

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React from "react";
+import React, { useContext } from "react";
 import {
   Sidebar,
   SidebarContent,
@@ -9,37 +8,28 @@ import {
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
+  SidebarMenuItem,
 } from "./sidebar";
-
-import { auth } from "@/firebase/firebaseConfig";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { UserContext } from "@/contexts/UserContext";
 import {
-  GoogleAuthProvider,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
-import { Avatar, AvatarImage } from "./avatar";
-import { AvatarFallback } from "@radix-ui/react-avatar";
-import { Button } from "./button";
-import Image from "next/image";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { ChevronsUpDown, LogOut } from "lucide-react";
+import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { signOut } from "firebase/auth";
+import { auth } from "@/firebase/firebaseConfig";
 
 export default function AppSidebar() {
-  const user = auth.currentUser;
+  const { userName, userEmail, userPhoto } = useContext(UserContext);
 
-  async function signoutUser() {
+  function logoutUser() {
     try {
       signOut(auth);
     } catch (err) {
-      console.log(err);
-    }
-  }
-
-  async function add() {
-    const provider = new GoogleAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-    } catch (error) {
-      console.error("Error signing in with Google:", error);
+      alert(err);
     }
   }
 
@@ -59,22 +49,38 @@ export default function AppSidebar() {
 
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuButton>
-            <span>
-              {user?.photoURL ? (
-                <Image
-                  className="size-6 rounded-full"
-                  src={user.photoURL}
-                  alt={`Profile pic of ${user.displayName}`}
-                  width={100}
-                  height={100}
-                />
-              ) : (
-                ""
-              )}
-            </span>
-            <p className="text-xs">{user?.displayName}</p>
-          </SidebarMenuButton>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <SidebarMenuButton asChild>
+                <div className="flex flex-row items-center gap-2">
+                  <Avatar className="size-6">
+                    <AvatarImage src={`${userPhoto}`} />
+
+                    <AvatarFallback>
+                      {userEmail?.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="flex flex-col items-start justify-center">
+                    <p className="text-xs">{userName}</p>
+                    <p className="text-xs text-muted-foreground">{userEmail}</p>
+                  </div>
+                  <ChevronsUpDown className="ml-auto" />
+                </div>
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent>
+              <DropdownMenuItem className="w-[--radix-popper-anchor-width]">
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton onClick={logoutUser}>
+                      Logout <LogOut className="ml-auto" />
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
