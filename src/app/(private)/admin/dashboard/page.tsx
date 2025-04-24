90ebdfbf617db7a32790854c01a802b71a98ba21
Raphael Elias <raphaeleliass@outlook.com>
2025-04-24 08:48:02 -0300

    feat: add Sidebar component with responsive design and links for admin dashboard

--- src/app/(private)/admin/dashboard/page.tsx
+++ src/app/(private)/admin/dashboard/page.tsx
@@ -1,5 +1,106 @@
-import React from "react";
+"use client";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+} from "@tabler/icons-react";
+import { LogOut, LogOutIcon } from "lucide-react";
+import { useState } from "react";
 
-export default function Page() {
-  return <div>teste123123</div>;
+export default function Dashboard() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const links = [
+    {
+      label: "Dashboard",
+      href: "#",
+      icon: (
+        <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Profile",
+      href: "#",
+      icon: (
+        <IconUserBolt className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Settings",
+      href: "#",
+      icon: (
+        <IconSettings className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: "Logout",
+      href: "#",
+      icon: (
+        <IconArrowLeft className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+  ];
+
+  return (
+    <div
+      className={cn(
+        "mx-auto flex w-full max-w-7xl flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
+        "h-dvh", // for your use case, use `h-screen` instead of `h-[60vh]`
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+            {open ? <LogOut /> : <LogOutIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <SidebarLink
+              link={{
+                label: "Manu Arora",
+                href: "#",
+                icon: (
+                  <img
+                    src="https://assets.aceternity.com/manu.png"
+                    className="h-7 w-7 shrink-0 rounded-full"
+                    width={50}
+                    height={50}
+                    alt="Avatar"
+                  />
+                ),
+              }}
+            />
+          </div>
+        </SidebarBody>
+      </Sidebar>
+      <div className="flex flex-1">
+        <div className="flex h-full w-full flex-1 flex-col gap-2 rounded-tl-2xl border border-neutral-200 bg-white p-2 md:p-10 dark:border-neutral-700 dark:bg-neutral-900">
+          <div className="flex gap-2">
+            {[...new Array(4)].map((i, idx) => (
+              <div
+                key={"first-array-demo-1" + idx}
+                className="h-20 w-full animate-pulse rounded-lg bg-gray-100 dark:bg-neutral-800"
+              ></div>
+            ))}
+          </div>
+          <div className="flex flex-1 gap-2">
+            {[...new Array(2)].map((i, idx) => (
+              <div
+                key={"second-array-demo-1" + idx}
+                className="h-full w-full animate-pulse rounded-lg bg-gray-100 dark:bg-neutral-800"
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
